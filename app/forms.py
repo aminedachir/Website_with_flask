5c5a2532c42ef568216840b4205f33bd199f5792
aminedachir <dachiramine110@gmail.com>
2021-04-28 15:05:15 +0100

    add firstname and password forms

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,10 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    firstname = StringField('firstname', validators=[DataRequired()])
+    lastname = StringField('lastname', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    confirm_email = StringField('confirm email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
