673bfd00532e8b96afa55cec3c73d774b3088403
aminedachir <dachiramine110@gmail.com>
2021-04-28 15:24:23 +0100

    if validate redirect to login page

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template
+from flask import render_template, redirect
 from app import app
 from app.forms import LoginForm
 
 @app.route('/')
 def home():
     return "Welcome"
-@app.route('/signup')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/login')
     return render_template('signup.html', form = form)
