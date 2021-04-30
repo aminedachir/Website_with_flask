4de8330b9824fde838b30766e2fd6d6b5e75b94f
aminedachir <dachiramine110@gmail.com>
2021-04-30 17:18:13 +0100

    Modifications

--- app/models.py
+++ app/models.py
@@ -1,21 +1,22 @@
+from datetime import datetime
 from app import db
 
-class user(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     firstname = db.Column(db.String(100), unique = True, index = True)
     lastname = db.Column(db.String(100), unique = True, index = True)
     password = db.Column(db.String(128))
     email = db.Column(db.String(120))
-    post = db.relationship('post',backref='author',lazy='dynamic')
+    posts = db.relationship('Post',backref='author',lazy='dynamic')
 
     def __repr__(self):
         return f"<user {self.email}>"
 
-class post(db.Model):
+class Posts(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime,index = True, default = datetime.utcnow)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f"<user {self.body}>"
+        return f"<Post {self.body}>"
