fe27c4cac2346a77e9a5bffdd453764aaf9f9dac
aminedachir <dachiramine110@gmail.com>
2021-04-30 17:10:04 +0100

    add table post

--- app/models.py
+++ app/models.py
@@ -9,3 +9,12 @@ class user(db.Model):
 
     def __repr__(self):
         return f"<user {self.email}>"
+
+class post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime,index = True, default = datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"<user {self.body}>"
