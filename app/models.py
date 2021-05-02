d2cbc2e70cb9010931914a6ef1f1b82629b71701
aminedachir <dachiramine110@gmail.com>
2021-05-02 23:40:49 +0100

    update

--- app/models.py
+++ app/models.py
@@ -11,3 +11,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User{self.firstname}>"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime,index = True, default = datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"<Post {self.body}>"
