843be9543b8a89fcdc0633194c63802b2feeed9d
aminedachir <dachiramine110@gmail.com>
2021-05-05 04:04:00 +0100

    Password Hashing with werkzeug

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
 from app import db
 
@@ -8,6 +9,11 @@ class User(db.Model):
     password = db.Column(db.String(128))
     email = db.Column(db.String(120))
     posts = db.relationship('Post',backref='author',lazy='dynamic')
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f"<User {self.firstname}>"
