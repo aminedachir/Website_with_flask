c892e3993172732f618b9cbccb2ba963fcaa2fbb
aminedachir <dachiramine110@gmail.com>
2021-05-07 00:16:16 +0100

    update

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, flash,url_for
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user,logout_user
@@ -9,23 +9,25 @@ from app.models import User
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit:
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
     return render_template('login.html', form = form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(firstname=form.firstname.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect('index')
-        return redirect(url_for('index'))
     return render_template('signup.html', form = form)
+
 @app.route('/logout')
 def logout():
     logout_user()
