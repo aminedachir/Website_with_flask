217729b9e1bca6bb21856a759c49b7b7876d13b8
aminedachir <dachiramine110@gmail.com>
2021-05-06 23:30:05 +0100

    update

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -9,7 +11,16 @@ def home():
 
 @app.route('/login')
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(firstname=form.firstname.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect('/login')
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', form = form)
 
 @app.route('/signup', methods=['GET', 'POST'])
