c78974ebc46714427c380192fb955bb7a088f9a3
aminedachir <dachiramine110@gmail.com>
2021-05-12 19:09:55 +0100

    make error if firstname invalid

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
-from flask import render_template, redirect
-from flask import flash
+from flask import render_template, redirect, flash
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user,logout_user
@@ -21,6 +20,9 @@ def signup():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(firstname=form.firstname.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect('/index')
         login_user(user)
         return redirect('/index')
     return render_template('signup.html', title='Sign In', form=form)
