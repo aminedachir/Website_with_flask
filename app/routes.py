3e58bbd3a5e154b79c485049e1019663378b66f9
aminedachir <dachiramine110@gmail.com>
2021-06-02 22:52:36 +0100

    login page and signup page

--- app/routes.py
+++ app/routes.py
@@ -10,14 +10,14 @@ def home():
     <a href='/signup'>Signup</a>
     '''
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        return '<h1>' + form.email.data + '</h1>'
     return render_template("login.html", form = form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>' + form.email.data + '</h1>'
     return render_template('signup.html', title='Sign In', form = form)
