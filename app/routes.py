a77d8ef0357d3f214eda06247a59f57ffcb62acc
aminedachir <dachiramine110@gmail.com>
2021-06-02 22:31:25 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
-from flask import render_template, redirect, flash
+from flask import render_template
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user,logout_user
 from app.models import User
 
 @app.route('/')
@@ -11,9 +10,11 @@ def home():
     <a href='/signup'>Signup</a>
     '''
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash("registred !!")
     return render_template('signup.html', title='Sign In', form=form)
