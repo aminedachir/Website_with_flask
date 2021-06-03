083047a2cd123881f2eefd1ae7298aea72e62136
aminedachir <dachiramine110@gmail.com>
2021-06-03 23:21:14 +0100

    add database

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "17@lames.com"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///home/amine/new_website/src/app.db' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
