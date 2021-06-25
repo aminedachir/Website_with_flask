7cbb6c42c62920773d856d2e71444bae3d0d97eb
aminedachir <dachiramine110@gmail.com>
2021-06-25 23:12:22 +0100

    multiplication and division pages

--- app/forms.py
+++ app/forms.py
@@ -2,16 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
 class LoginForm(FlaskForm):
     firstname = StringField('firstname', validators=[DataRequired(), Length(min=4, max=8)])
     email = StringField('email', validators=[DataRequired(), Length(min=8,max=80)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
 
+
 class LoginForm2(FlaskForm):
     email = StringField('email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
+
 
 class LoginForm3(FlaskForm):
     email = StringField('email', validators=[DataRequired()])
